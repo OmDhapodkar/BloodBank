0329a8b8eed81e42d4563daf1c4dece1b63e7337
OmDhapodkar <om.dhapodkar2020@vitstudent.ac.in>
2023-05-24 18:52:44 +0530

    First Change

--- app/src/main/java/com/example/bloodbank/MainActivity.kt
+++ app/src/main/java/com/example/bloodbank/MainActivity.kt
@@ -53,6 +53,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BloodBankTheme {
                 // A surface container using the 'background' color from the theme
+                // GitHub Sync Practice
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
